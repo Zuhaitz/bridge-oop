5f0a05fe9a7cdf8ee35af7af259e7133fbcc2cca
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-05-17 19:27:39 +0200

    Kata 2 finished

--- katas.js
+++ katas.js
@@ -43,3 +43,52 @@ maya.drinkColaCao();
 
 aztec.defend(maya.attack());
 maya.defend(aztec.attack());
+
+// Kata 2 - Il sogno di volare
+class Gallery {
+  constructor(civil, militar) {
+    this.civil = civil;
+    this.militar = militar;
+  }
+
+  getRandomCivil() {
+    return this.civil[Math.floor(Math.random * this.civil.length)];
+  }
+
+  getRandomMilitar() {
+    return this.militar[Math.floor(Math.random * this.militar.length)];
+  }
+
+  getAll() {
+    return this.civil.concat(this.militar);
+  }
+}
+
+class Painter {
+  constructor() {
+    createGallery();
+  }
+
+  createGallery() {
+    let section = document.createElement("section");
+    document.getElementsByTagName("body")[0].appendChild(section);
+    this.gallery = section;
+  }
+
+  createImageTag(url) {
+    let pic = document.createElement("picture");
+    let img = document.createElement("img");
+    img.setAttribute("src", url);
+    pic.appendChild(img);
+
+    return pic;
+  }
+
+  paintSingleImage(url) {
+    this.gallery.appendChild(this.createImageTag(url));
+  }
+
+  paintMultipleImages(images) {
+    images.forEach((img) => this.gallery.appendChild(this.createImageTag(img)));
+  }
+}
